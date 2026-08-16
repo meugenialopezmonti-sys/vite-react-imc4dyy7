f4df4fd738ce2f309c5def102866240f3a0640c8
meugenialopezmonti-sys <m.eugenialopezmonti@gmail.com>
2026-08-16 17:03:05 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'reset.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
